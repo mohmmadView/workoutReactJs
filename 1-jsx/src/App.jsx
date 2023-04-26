9a54e8e4895065dfefc7edb8d087a71756aa364a
darksouls <darksouls@localhost.localdomain>
2023-04-26 22:06:47 +0330

    finsh 1-jsx

--- 1-jsx/src/App.jsx
+++ 1-jsx/src/App.jsx
@@ -14,21 +14,34 @@ function App() {
 <h1 className=" text-3xl font-bold underline p-4 text-center mt-3 ">What is a Jsx <span className='text-green-500 text-5xl font-mono font-extrabold'> ?</span></h1>
 <section className='flex justify-center'>
 <div className="w-3/4  bg-slate-700 p-8 border "><p className='text-justify mb-6'>JSX stands for JavaScript XML. It allows writing HTML in
- JavaScript and converts the HTML tags into React elements.</p>
- <ul className="list-disc pb-6 pl-6 text-green-400">
+ JavaScript and converts the HTML tags into React elements.
+ <br/> <span className='text-yellow-600 font-bold text-2xl'> JSX </span> ( <span className='text-xl text-green-400'> JavaScript Extension</span> )<br/>
+  is a React extension which allows writing JavaScript code that looks like HTML. In other words, JSX is an HTML-like syntax used by React that extends ECMAScript so that HTML-like syntax can co-exist with JavaScript/React code. The syntax is used by preprocessors (i.e., transpilers like babel) to transform HTML-like syntax into standard JavaScript objects that a JavaScript engine will parse.</p>
+  
+ <ul className="list-disc pb-6 pl-6  text-green-400">
   <li className=' '>JSX stands for JavaScript XML.</li>
   <li className=' '>JSX allows us to write HTML in React.</li>
   <li className=' '>JSX makes it easier to write and add HTML in React.</li>
  </ul>
+ <h1 className='text-2xl p-4 text-white'>JSX file :</h1>
  <div className="mockup-code">
   <pre data-prefix="$"><code className=""><span className="text-purple-400">const</span> element <span className=" ">=</span> <span className="token tag"><span className="token tag"><span className="token punctuation">&lt;</span>h1</span><span className="token punctuation">&gt;</span></span><span className="token plain-text">Hello, world!</span><span className="token tag"><span className="token tag"><span className="token punctuation">&lt;/</span>h1</span><span className="token punctuation">&gt;</span></span><span className="token punctuation">;</span></code></pre> 
-  <pre data-prefix="$" className=""><span class="token keyword text-purple-400">const</span> root <span class="token operator">=</span> ReactDOM<span class="token punctuation">.</span><span class="token function">createRoot</span><span class="token punctuation">(</span>document<span class="token punctuation">.</span><span class="token function">getElementById</span><span class="token punctuation">(</span><span class="token string">'root'</span><span class="token punctuation">)</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+  <pre data-prefix="$" className=""><span className="token keyword text-purple-400">const</span> root <span className="token operator">=</span> ReactDOM<span className="token punctuation">.</span><span className="token function">createRoot</span><span className="token punctuation">(</span>document<span className="token punctuation">.</span><span className="token function">getElementById</span><span className="token punctuation">(</span><span className="token string">'root'</span><span className="token punctuation">)</span><span className="token punctuation">)</span><span className="token punctuation">;</span>
 
 </pre> 
-  <pre data-prefix=">" className="text-success"><code>root<span class="token punctuation">.</span><span class="token function">render</span><span class="token punctuation">(</span>myElement<span class="token punctuation">)</span><span class="token punctuation">;</span>
+  <pre data-prefix=">" className="text-success"><code>root<span className="token punctuation">.</span><span className="token function">render</span><span className="token punctuation">(</span>myElement<span className="token punctuation">)</span><span className="token punctuation">;</span>
 </code></pre>
 </div>
+
+ <h1 className='text-2xl p-4 text-white'>Corresponding Output  :</h1>
+  <div className="mockup-code">
+
+<pre data-prefix=''><span>React.<span className='text-yellow-500'>createElement(</span><span className="text-green-500">"div"</span><span>,&nbsp;</span><span className="text-red-600">null</span><span>,&nbsp;</span><span className="text-green-500">"Hello&nbsp;JavaTpoint"</span><span>);&nbsp;&nbsp;</span></span>
+</pre>
+</div>
+<p className='mt-5'>The above line creates a react element and passing three arguments inside where the first is the name of the element which is div, second is the attributes passed in the div tag, and last is the content you pass which is the "Hello JavaTpoint."</p>
  </div>
+
 </section>
    
 
--- 2.Componet/src/index.js
+++ 2.Componet/src/index.js
@@ -7,7 +7,7 @@ import Comment  from './Comment';
 class ReactComponent extends React.Component {
   render() {
     return (
-    <div className='text-red-800 font-bold bg-black text-center pt-5 bg-black text-5xl'>1-React
+    <div className='text-red-800 font-bold bg-black text-center pt-5 bg-black text-5xl'>2-React
      <span className='bg-slate-800 px-2 border border-blue-300 rounded-md  text-blue-500'> {this.props.name} </span>
      <Comment subject="what a component is ?" title="Components are independent, reusable bits of code. They serve the same purpose as JavaScript functions, but they work separately and return HTML. Components are in two types: Class Components and Function Components, in this first tutorial we focus on Function Components."/>
      <App />
