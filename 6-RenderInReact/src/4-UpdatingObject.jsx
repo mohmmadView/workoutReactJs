d996c20384b48e14cb174d57f74ce0920370f732
mohmmadView <mamad.rahgoshahi@gmail.com>
2023-10-22 02:01:56 +0330

    update--6-Rendar__updateObject

--- 6-RenderInReact/src/4-UpdatingObject.jsx
+++ 6-RenderInReact/src/4-UpdatingObject.jsx
@@ -1,21 +1,26 @@
 import { useState } from 'react';
 export default function MovingDot() {
+
   const [position, setPosition] = useState({
     x: 0,
     y: 0
   });
   return (
-    <div className='w-full h-screen relative bg-slate-500'
+    <div className='w-1/2  relative bg-slate-500 mt-4  mx-8'
       onPointerMove={e => {
+        e.target.onmouseover=(e)=>{console.log(e);};
         setPosition({
-          x: e.clientX,
-          y: e.clientY
+          x: e.nativeEvent.layerX,
+          y: e.nativeEvent.layerY
         });
+        console.log(e, e.nativeEvent.layerY,e.nativeEvent.offsetX,e.nativeEvent.clientX);
       }}
       >
-      <div className={`absolute bg-red-500 rounded-full transform translate-x-${`{${position.x}px}`} transform translate-y-${`{${position.y}px}`}  h-3 w-3`}  style={{
+      <div className={`absolute bg-red-500 rounded-full  h-3 w-3`} 
+       style={{
         transform: `translate(${position.x}px, ${position.y}px)`,
-      }}  />
+      }} 
+       />
     </div>
   );
 }
--- 6-RenderInReact/src/App.jsx
+++ 6-RenderInReact/src/App.jsx
@@ -1,12 +1,12 @@
-import StageShot from './1-snapShot';
-import StateUpdate from './2-StateUpdate';
-import StateUpdateTrue from './3-StateUpdateTrue';
-import UpdatingObject from './4-UpdatingObject';
-import  Title from './utils/Title';
-import Example from './utils/Example';
-import Code from './utils/Prism';
+import StageShot from "./1-snapShot";
+import StateUpdate from "./2-StateUpdate";
+import StateUpdateTrue from "./3-StateUpdateTrue";
+import UpdatingObject from "./4-UpdatingObject";
+import Title from "./utils/Title";
+import Example from "./utils/Example";
+import Code from "./utils/Prism";
 function App() {
-let stateUpdateCode =`
+  let stateUpdateCode = `
 import { useState } from 'react';
 export default function Counter() {
     const [number, setNumber] = useState(0);
@@ -24,7 +24,7 @@ export default function Counter() {
   }
   
 `;
-let stateUpdateCodeTrue =`
+  let stateUpdateCodeTrue = `
 import { useState } from 'react';
 export default function Counter__true() {
     const [number, setNumber] = useState(0);
@@ -41,89 +41,119 @@ export default function Counter__true() {
     )
   }
   
-`
+`;
   return (
     <>
-    <Title  title="Render in React" />
-    <div className='flex justify-center'>
-      <p className='w-8/12 my-6 text-2xl text-primary bg-secondary p-8 rounded-lg'>
-      Before your components are displayed on screen, they must be rendered by React. Understanding the steps in this process will help you think about how your code executes and explain its behavior.
-      </p>
-    </div>
+      <Title title="Render in React" />
+      <div className="flex justify-center">
+        <p className="w-8/12 my-6 text-2xl text-primary bg-secondary p-8 rounded-lg">
+          Before your components are displayed on screen, they must be rendered
+          by React. Understanding the steps in this process will help you think
+          about how your code executes and explain its behavior.
+        </p>
+      </div>
       <div className="flex justify-center overflow-x-auto">
-          <table className="w-6/12 table">
+        <table className="w-6/12 table">
           {/* head */}
-          <thead>
-          </thead>
+          <thead></thead>
           <tbody>
             <tr>
               <th>1</th>
-              <td className='text-lime-400 font-bold text-xl'>Triggering</td>
-              <td className='text-blue-400 text-xl'>a render (delivering the guest’s order to the kitchen)</td>
+              <td className="text-lime-400 font-bold text-xl">Triggering</td>
+              <td className="text-blue-400 text-xl">
+                a render (delivering the guest’s order to the kitchen)
+              </td>
             </tr>
             <tr>
               <th>2</th>
-              <td className='text-lime-400 font-bold text-xl'>Rendering</td>
-              <td className='text-blue-400 text-xl'> the component (preparing the order in the kitchen)</td>
+              <td className="text-lime-400 font-bold text-xl">Rendering</td>
+              <td className="text-blue-400 text-xl">
+                {" "}
+                the component (preparing the order in the kitchen)
+              </td>
             </tr>
             <tr>
               <th>3</th>
-              <td className='text-lime-400 font-bold text-xl'>Committing</td>
-              <td className='text-blue-400 text-xl'>to the DOM (placing the order on the table)</td>
+              <td className="text-lime-400 font-bold text-xl">Committing</td>
+              <td className="text-blue-400 text-xl">
+                to the DOM (placing the order on the table)
+              </td>
             </tr>
           </tbody>
         </table>
       </div>
-    <Example  exampleNumber="Step 1" exampleName="Trigger a render" />
-    <div className='flex flex-col  align-middle'>
-      <p className='w-8/12 mt-6 text-3xl text-primary bg-secondary text-center self-center p-4 '>
-      There are two reasons for a component to render: 
-      </p><br/>
-       <p className='w-8/12 text-2xl text-red-400 bg-secondary p-2 self-center '>
-      1. It’s the component’s initial render.
-      </p>
-       <p className='w-8/12 text-2xl  text-red-400 bg-secondary p-2 self-center '>
-      2. The component’s (or one of its ancestors’) state has been updated.
-      </p>
-    </div>
-    <Example  exampleNumber="Example Render" exampleName="State as a Snapshot" />
-    <div className='w-full my-4  flex justify-center'>
-       <StageShot />
-    </div>
-    <Title title="Queueing a Series of State Updates" />
-      <div className='flex justify-center'>
-      <p className='w-8/12 my-6 text-2xl text-primary bg-secondary p-8 rounded-lg'>
-      Setting a state variable will queue another render. But sometimes you might want to perform multiple operations on the value before queueing the next render. To do this, it helps to understand how React batches state updates.</p>
-    </div>
-    <Example exampleName="React batches state updates" exampleNumber="Example  " />
-    <div className='flex justify-center'>
-      <p className='w-8/12 my-6 text-2xl text-primary bg-secondary p-8 rounded-lg'>
-      You might expect that clicking the “+3” button will increment the counter three times because it calls setNumber(number + 1) three times:
-      </p>
-    </div>
-    <div className='flex justify-center'>
-    <StateUpdate />
-    <Code  code={stateUpdateCode} language="js" />
-    </div> 
-    <Example exampleNumber="" exampleName="Updating the same state multiple times before the next render" />
-     <div className='flex justify-center'>
-      <p className='w-8/12 my-6 text-2xl text-primary bg-secondary p-8 rounded-lg'>
-          It is an uncommon use case, but if you would like to update the same state variable multiple times before the next render, instead of passing the next state value like setNumber(number + 1), you can pass a function that calculates the next state based on the previous one in the queue, like setNumber(n => n + 1). It is a way to tell React to “do something with the state value” instead of just replacing it.
-         Try incrementing the counter now:
-      </p>   
-       </div>
-      <div className='flex justify-center'>
-     <StateUpdateTrue />
-      <Code  code={stateUpdateCodeTrue} language="js" />
+      <Example exampleNumber="Step 1" exampleName="Trigger a render" />
+      <div className="flex flex-col  align-middle">
+        <p className="w-8/12 mt-6 text-3xl text-primary bg-secondary text-center self-center p-4 ">
+          There are two reasons for a component to render:
+        </p>
+        <br />
+        <p className="w-8/12 text-2xl text-red-400 bg-secondary p-2 self-center ">
+          1. It’s the component’s initial render.
+        </p>
+        <p className="w-8/12 text-2xl  text-red-400 bg-secondary p-2 self-center ">
+          2. The component’s (or one of its ancestors’) state has been updated.
+        </p>
       </div>
-     
-        <Example exampleNumber="" exampleName="Update Object" />
-        <div className='flex justify-center'>
-      <UpdatingObject  />
-        </div>
+      <Example
+        exampleNumber="Example Render"
+        exampleName="State as a Snapshot"
+      />
+      <div className="w-full my-4  flex justify-center">
+        <StageShot />
+      </div>
+      <Title title="Queueing a Series of State Updates" />
+      <div className="flex justify-center">
+        <p className="w-8/12 my-6 text-2xl text-primary bg-secondary p-8 rounded-lg">
+          Setting a state variable will queue another render. But sometimes you
+          might want to perform multiple operations on the value before queueing
+          the next render. To do this, it helps to understand how React batches
+          state updates.
+        </p>
+      </div>
+      <Example
+        exampleName="React batches state updates"
+        exampleNumber="Example  "
+      />
+      <div className="flex justify-center">
+        <p className="w-8/12 my-6 text-2xl text-primary bg-secondary p-8 rounded-lg">
+          You might expect that clicking the “+3” button will increment the
+          counter three times because it calls setNumber(number + 1) three
+          times:
+        </p>
+      </div>
+      <div className="flex justify-center">
+        <StateUpdate />
+        <Code code={stateUpdateCode} language="js" />
+      </div>
+      <Example
+        exampleNumber=""
+        exampleName="Updating the same state multiple times before the next render"
+      />
+      <div className="flex justify-center">
+        <p className="w-8/12 my-6 text-2xl text-primary bg-secondary p-8 rounded-lg">
+          It is an uncommon use case, but if you would like to update the same
+          state variable multiple times before the next render, instead of
+          passing the next state value like setNumber(number + 1), you can pass
+          a function that calculates the next state based on the previous one in
+          the queue, like setNumber(n => n + 1). It is a way to tell React to
+          “do something with the state value” instead of just replacing it. Try
+          incrementing the counter now:
+        </p>
+      </div>
+      <div className="flex justify-center">
+        <StateUpdateTrue />
+        <Code code={stateUpdateCodeTrue} language="js" />
+      </div>
+
+      <Example exampleNumber="" exampleName="Update Object" />
+      <div className="flex justify-center">
+        <UpdatingObject />
+         <Code code={stateUpdateCodeTrue} language="js" />
+      </div>
+       <Example exampleNumber="" exampleName="Update Object" />
     </>
-    
-  )
+  );
 }
 
-export default App
+export default App;
