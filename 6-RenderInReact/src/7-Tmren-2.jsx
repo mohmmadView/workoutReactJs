c59d457dda753b07a417eb5ffa1114a811dd2189
mohmmadView <mamad.rahgoshahi@gmail.com>
2023-11-18 12:12:32 +0330

    update the end 6

--- 6-RenderInReact/src/7-Tmren-2.jsx
+++ 6-RenderInReact/src/7-Tmren-2.jsx
@@ -1,42 +1,144 @@
 import {useState} from 'react'
 
-const dataList = [
-    {
-        id: 1,
-        Todo: "read Book Today",
-        checkList: false
-    },
-    {
-        id: 2,
-        Todo: "work in project",
-        checkList: true
-    },
-    {
-        id: 3,
-        Todo: "guide in React",
-        checkList: true
-    }
+
+
+let nextId = 3;
+const initialTodos = [
+  { id: 0, title: 'Buy milk', done: true },
+  { id: 1, title: 'Eat tacos', done: false },
+  { id: 2, title: 'Brew tea', done: false },
 ];
 
-export default function Tmren_2 (){
-  const [list , setList] = useState(dataList);
+export default function Tmren2() {
+  const [todos, setTodos] = useState(
+    initialTodos
+  );
+  function TaskList({
+  todos,
+  onChangeTodo,
+  onDeleteTodo
+}) {
+  return (
+    <ul className='flex flex-col '>
+      {todos.map(todo => (
+        <li className='flex justify-between' key={todo.id}>
+          <Task
+            todo={todo}
+            onChange={onChangeTodo}
+            onDelete={onDeleteTodo}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+}
 
-    return(
-        <div className='w-1/2 flex flex-col mt-8'>
-            <div className='flex row gap-2 mb-6'>
-                <input className='input input-secondary bg-green-200 w-full'></input>
-                <div className='btn btn-success '> Add </div>
-            </div>
-            {dataList.map((item)=>{
-                return(
-                    <li className='flex row justify-between mb-4 ' key={item.id}>
-                        <input type="checkbox" checked={item.checkList} className="checkbox" />
-                        <h1 className='text-2xl'>{item.Todo}</h1>  
-                        <div className='btn btn-success'>edit</div> 
-                        <div className='btn btn-success'>Delete</div>
-                     </li>
-                )
-            })}
-        </div>
+function Task({ todo, onChange, onDelete }) {
+  const [isEditing, setIsEditing] = useState(false);
+  let todoContent;
+  if (isEditing) {
+    todoContent = (
+      <>
+        <input 
+        className='input input-warning'
+          value={todo.title}
+          onChange={e => {
+            onChange({
+              ...todo,
+              title: e.target.value
+            });
+          }} />
+        <button className='btn btn-primary ' onClick={() => setIsEditing(false)}>
+          Save
+        </button>
+      </>
+    );
+  } else {
+    todoContent = (
+      <>
+        {todo.title}
+        <button className='btn btn-info ml-4' onClick={() => setIsEditing(true)}>
+          Edit
+        </button>
+      </>
+    );
+  }
+  return (
+    <label>
+      <input
+        className=' checkbox checkbox-secondary'
+        type="checkbox"
+        checked={todo.done}
+        onChange={e => {
+          onChange({
+            ...todo,
+            done: e.target.checked
+          });
+        }}
+      />
+      {todoContent}
+      <button className='btn btn-accent ml-4' onClick={() => onDelete(todo.id)}>
+        Delete
+      </button>
+    </label>
+  );
+}
+
+  function AddTodo({ onAddTodo }) {
+    const [title, setTitle] = useState('');
+    return (
+      <div className=''>
+        <input
+          className='input input-success'
+          placeholder="Add todo"
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
+        <button className='btn btn-primary h-auto' onClick={() => {
+          setTitle('');
+          onAddTodo(title);
+        }}>Add</button>
+      </div>
     )
+  }
+  
+  function handleAddTodo(title) {
+    setTodos([
+      ...todos,
+      {
+        id: nextId++,
+        title: title,
+        done: false
+      }
+    ]);
+  }
+
+  function handleChangeTodo(nextTodo) {
+    setTodos(todos.map(t => {
+      if (t.id === nextTodo.id) {
+        return nextTodo;
+      } else {
+        return t;
+      }
+    }));
+  }
+
+  function handleDeleteTodo(todoId) {
+    setTodos(
+      todos.filter(t => t.id !== todoId)
+    );
+  }
+
+  return (
+    <div className=' flex flex-col'>
+      <AddTodo
+        onAddTodo={handleAddTodo}
+      />
+      <TaskList
+        todos={todos}
+        onChangeTodo={handleChangeTodo}
+        onDeleteTodo={handleDeleteTodo}
+      />
+    </div>
+  );
 }
