59d06b118ee3217ae3ec23f343202a942eb66205
mohmmadView <mamad.rahgoshahi@gmail.com>
2023-11-18 13:23:53 +0330

    add task

---
+++ 6-5-Task_Note/src/Add_task.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+
+export default  function Add_task(){
+let  [title ,setTitle] = useState("");
+
+    return(
+        <section>
+          <input className="input__Add" onChange={(e)=>setTitle(e.target.value)} value={title} />
+           <button className="btn__Add" onClick={()=>{
+            
+               console.log(title)
+            
+           }} >Add Task</button>
+        </section>
+    )
+}
