785e651211071dd55046a46bab99cd724d24b08c
mohmmadView <mamad.rahgoshahi@gmail.com>
2023-12-03 16:48:59 +0330

    ctrate 7-managingState

---
+++ 7-ManagingState/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <button className='btn btn-success'>test</button>
+      <div className='bg-primary w-14 h-64'>okey</div>
+    </>
+  )
+}
+
+export default App
