d75edf8f7c6099b61de1150e97202a30c2b6bf00
mohmmadView <mamad.rahgoshahi@gmail.com>
2023-12-05 15:00:28 +0330

    update example new

--- 7-ManagingState/src/1-CityQiuz.jsx
+++ 7-ManagingState/src/1-CityQiuz.jsx
@@ -1,45 +1,150 @@
-import { useState } from "react"
+import { useState } from 'react';
+import Prism from './utils/Prism';
+export default function Form() {
+  let CodeCityQiuz = `
+  import { useState } from 'react';
 
-export default function CityQuiz() {
-     const cities = [
-        "New York",
-        "Los Angeles",
-        "Chicago",
-        "Houston",
-        "Phoenix",
-        "Philadelphia",
-        "San Antonio",
-        "San Diego",
-        "Dallas",
-        "San Jose",
-    ]
-    let [answer,setAnswer]=useState("");
-    let [disable,setDisable]=useState(false);
-    let [message,setMessage] = useState("");
-    function textareaHandleChange(e){
-        setAnswer(e.target.value);
-        setDisable(false);
-    }
-    function handleSubmit(){
-        setDisable(true);
-        let i;
-       for(i=0;i<cities.length;i++){
-  if(cities[i].toLowerCase().split(" ").join("")===answer.toLowerCase().split(" ").join("")){
-    setMessage("answer is Good");
-   break;
-  }else{
-    setMessage("answer is wrong");
+export default function Form() {
+  const [answer, setAnswer] = useState('');
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState('typing');
+
+  if (status === 'success') {
+    return <h1>That's right!</h1>
   }
-       }
-    }   
-return (
-    <div className="w-1/3   p-4 flex flex-col justify-between align-baseline ">
+  
 
-        <h1 className="text-3xl font-bold text-primary">City Quiz</h1>
-        <p>It is one oanswerf the big cities of usa. The answer to the question?</p>
-        <textarea onChange={textareaHandleChange} className="textarea textarea-secondary textarea-lg"/>
-        <p className={`${message === "answer is Good" ? 'text-green-500' : 'text-red-500'}   `}>{message}</p>
-        <button onClick={handleSubmit} className={`btn btn-secondary w-1/3 self-center ${disable && 'btn-disabled'}`}>Submit</button>
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setStatus('submitting');
+    try {
+      await submitForm(answer);
+      setStatus('success');
+    } catch (err) {
+      setStatus('typing');
+      setError(err);
+    }
+  }
+
+  function handleTextareaChange(e) {
+    setAnswer(e.target.value);
+  }
+
+  return (
+    <div className='w-10/12 mx-auto my-12 bg-base-300 p-4 text-xl flex' >
+    <div className='w-1/3 bg-base-100 p-4 border '>
+      <h2 className='text-3xl p-2 text-primary'>City quiz</h2>
+      <p>
+        In which city is there a billboard that turns air into drinkable water?
+      </p>
+      <form className='flex flex-col' onSubmit={handleSubmit}>
+        <textarea className=' textarea textarea-lg textarea-secondary my-4' 
+          value={answer}
+          onChange={handleTextareaChange}
+          disabled={status === 'submitting'}
+        />
+        <br />
+        <button className='btn btn-secondary m-auto self-center' disabled={
+          answer.length === 0 ||
+          status === 'submitting'
+        }>
+          Submit
+        </button>
+      <p className={text-xs self-end {status==="submitting"?"text-warning":"text-success"} {answer.length === 0?"hidden":"block"}}>{status}</p>
+        {error !== null &&
+          <p className="Error">
+            {error.message}
+          </p>
+        }
+      </form>
     </div>
-)
+    </div>
+  );
+}
+
+function submitForm(answer) {
+  // Pretend it's hitting the network.
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let shouldError = answer.toLowerCase() !== 'lima'
+      if (shouldError) {
+        reject(new Error('Good guess but a wrong answer. Try again!'));
+      } else {
+        resolve();
+      }
+    }, 1500);
+  });
+}
+  `
+  const [answer, setAnswer] = useState('');
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState('typing');
+
+  if (status === 'success') {
+    return <h1>That's right!</h1>
+  }
+  
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setStatus('submitting');
+    try {
+      await submitForm(answer);
+      setStatus('success');
+    } catch (err) {
+      setStatus('typing');
+      setError(err);
+    }
+  }
+
+  function handleTextareaChange(e) {
+    setAnswer(e.target.value);
+  }
+
+  return (
+    <div className='w-10/12 mx-auto my-12 bg-base-300 p-4 text-xl flex' >
+    <div className='w-1/3 bg-base-100 p-4'>
+      <h2 className='text-3xl p-2 text-primary'>City quiz</h2>
+      <p>
+        In which city is there a billboard that turns air into drinkable water?
+      </p>
+      <form className='flex flex-col' onSubmit={handleSubmit}>
+        <textarea className=' textarea textarea-lg textarea-secondary my-4' 
+          value={answer}
+          onChange={handleTextareaChange}
+          disabled={status === 'submitting'}
+        />
+        <br />
+        <button className='btn btn-secondary m-auto self-center' disabled={
+          answer.length === 0 ||
+          status === 'submitting'
+        }>
+          Submit
+        </button>
+      <p className={`text-xs self-end ${status==="submitting"?"text-warning":"text-success"} ${answer.length === 0?"hidden":"block"}`}>{status}</p>
+        {error !== null &&
+          <p className="Error">
+            {error.message}
+          </p>
+        }
+      </form>
+    </div>
+    
+<Prism   language="js" code={CodeCityQiuz} />
+    </div>
+  );
+}
+
+function submitForm(answer) {
+  // Pretend it's hitting the network.
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let shouldError = answer.toLowerCase() !== 'lima'
+      if (shouldError) {
+        reject(new Error('Good guess but a wrong answer. Try again!'));
+      } else {
+        resolve();
+      }
+    }, 1500);
+  });
 }
--- 7-ManagingState/src/App.jsx
+++ 7-ManagingState/src/App.jsx
@@ -1,62 +1,15 @@
 import { useState } from 'react'
 import Title from "./utils/Title";
 import Example from "./utils/Example";
-import Code from "./utils/Prism";
 import CityQiuz from './1-CityQiuz';
+import CityQuiz2 from './2-CityQiuz';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css';
 import './index.css';
 
 function App() {
-  const CodeCityQiuz = `
-  import { useState } from "react"
-
-export default function CityQuiz() {
-     const cities = [
-        "New York",
-        "Los Angeles",
-        "Chicago",
-        "Houston",
-        "Phoenix",
-        "Philadelphia",
-        "San Antonio",
-        "San Diego",
-        "Dallas",
-        "San Jose",
-    ]
-    let [answer,setAnswer]=useState("");
-    let [disable,setDisable]=useState(false);
-    let [message,setMessage] = useState("");
-    function textareaHandleChange(e){
-        setAnswer(e.target.value);
-        setDisable(false);
-    }
-    function handleSubmit(){
-        setDisable(true);
-        console.log(answer);
-        let i;
-       for(i=0;i<cities.length;i++){
-  if(cities[i].toLowerCase()===answer.toLowerCase()){
-    setMessage("answer is Good");
-   break;
-  }else{
-    setMessage("answer is wrong");
-  }
-       }
-    }   
-return (
-    <div className="w-1/2   p-4 flex flex-col justify-between align-baseline ">
-
-        <h1 className="text-3xl font-bold text-primary">City Quiz</h1>
-        <p>It is one of the big cities of usa. The answer to the question?</p>
-        <textarea onChange={textareaHandleChange} className="textarea textarea-secondary textarea-lg"/>
-        <p className={{message === "answer is Good" ? 'text-green-500' : 'text-red-500'}    text-red-500  'text-green-500'}>{message}</p>
-        <button onClick={handleSubmit} className={btn btn-secondary w-1/3 self-center {disable && btn-disabled}}>Submit</button>
-    </div>
-)
-}
-  `
+  
 
   return (
     <div className='w-full'>
@@ -77,14 +30,21 @@ return (
      <div className='w-10/12 mx-auto my-12 bg-base-300 p-10 text-xl' >
          <div className="divider divider-warning text-secondary font-bold text-3xl"> Reacting to input with state </div>
        <p>
-       With React, you won’t modify the UI from code directly. For example, you won’t write commands like “disable the button”, “enable the button”, “show the success message”, etc. Instead, you will describe the UI you want to see for the different visual states of your component (“initial state”, “typing state”, “success state”), and then trigger the state changes in response to user input. This is similar to how designers think about UI.
+       With <span className='text-blue-400'>React</span>, you won’t modify the UI from code directly. For example, you won’t write commands like “disable the button”, “enable the button”, “show the success message”, etc. Instead, you will describe the UI you want to see for the different visual states of your component <span>(“initial state”, “typing state”, “success state”),</span> and then trigger the state changes in response to user input. This is similar to how designers think about UI.
+       Here is a quiz form built using React. Note how it uses the status state variable to determine whether to enable or disable the submit button, and whether to show the success message instead.       </p>
+     </div>
+          <div className='w-10/12 mx-auto my-12 bg-base-300 p-10 text-xl' >
+         <div className="divider divider-warning text-secondary font-bold text-3xl"> واکنش به ورودی با حالت </div>
+       <p>
+       با <span className='text-blue-400'>React</span>، شما از کد به صورت مستقیم تغییرات را در رابط کاربری اعمال نخواهید کرد. به عنوان مثال، دستوراتی مانند "دکمه را غیرفعال کن"، "دکمه را فعال کن"، "پیام موفقیت را نشان بده" و غیره را نخواهید نوشت. به جای اینکه دستورات مستقیم بنویسید، شما توصیف خواهید کرد که رابط کاربری مورد نظر خود را برای وضعیت‌های مختلف دیداری کامپوننت خود <span>("وضعیت اولیه"، "وضعیت تایپ کردن"، "وضعیت موفقیت") </span>و سپس تغییرات وضعیت را در پاسخ به ورودی کاربر فراخوانی خواهید کرد. این مشابه به این است که طراحان به رابط کاربری فکر می‌کنند.
 
-Here is a quiz form built using React. Note how it uses the status state variable to determine whether to enable or disable the submit button, and whether to show the success message instead.       </p>
+در زیر، یک فرم آزمون به کمک React ساخته شده است. توجه داشته باشید که از متغیر وضعیت <span>(status)</span> برای تعیین اینکه آیا دکمه ارسال فعال یا غیرفعال باشد و آیا پیام موفقیت را نشان دهد یا خیر، استفاده شده است   </p>
      </div>
-      <div className='w-10/12 mx-auto my-12 bg-base-300 p-4 text-xl flex' >
-        <CityQiuz/>
-        <Code language="js" code={CodeCityQiuz} />
-     </div>
+      <CityQiuz />
+     <CityQuiz2 />
+      
+       
+       
      </div>
   )
 }
