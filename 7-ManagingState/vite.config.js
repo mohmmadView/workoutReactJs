742ecfd50b0eeced6e50016de1cd7a6909aaf96b
mohmmadView <mamad.rahgoshahi@gmail.com>
2023-12-19 21:43:30 +0330

    config network true

--- 7-ManagingState/vite.config.js
+++ 7-ManagingState/vite.config.js
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+host: '0.0.0.0',
+port:"8020",
+  },
   plugins: [react()],
+ 
 });
