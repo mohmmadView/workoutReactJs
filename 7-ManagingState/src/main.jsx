25427b25f39dbe43e25f12db274bdbb94fd2e426
mohmmadView <mamad.rahgoshahi@gmail.com>
2023-12-29 04:19:13 +0330

    fix bug testSpeed varcel

--- 7-ManagingState/src/main.jsx
+++ 7-ManagingState/src/main.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  <SpeedInsights>
     <main className="w-11/12 flex m-auto">
       <App />
     </main>
-    </SpeedInsights>
+    <SpeedInsights />
   </React.StrictMode>,
 );
