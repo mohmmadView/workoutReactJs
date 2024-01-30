a60ffd6600581b942152303314b6050df3494e80
mohmmadView <mamad.rahgoshahi@gmail.com>
2024-01-30 04:36:08 +0330

    update finesh eng and fa  transliter

--- 7-ManagingState/src/App.jsx
+++ 7-ManagingState/src/App.jsx
@@ -23,69 +23,56 @@ export default function App() {
     <div className="w-full">
       <Title title="Managing State" />
       <button className="btn btn-outline  mx-2" onClick={()=>{
-        setLang(true) 
+        setLang(false) 
         }}>fa</button>
       <button className="btn btn-outline  mx-2" onClick={()=>{
-         setLang(false)
+         setLang(true)
       }}>En</button>
-     {lang ?  <Container Title={"Managing State"}>{DataText[0].Intermediate()}</Container>: <Container Title={"مدیریت وضعیت"}>{DataText[1].ManagingState()}</Container> }
-      {/* <Container  Title={"Intermediate"}>{DataText[0].Intermediate()}</Container>
-      <Container Title={"مدیریت وضعیت"}>{DataText[1].ManagingState()}
-      </Container> */}
+     {lang ?  <Container Title={"Managing State"}>{DataText[0].Intermediate()}</Container>: 
+     <Container Title={"مدیریت وضعیت"}>{DataText[1].ManagingState()}</Container> }
+
       <Example exampleNumber={1} exampleName={"Example State"} />
+
       {lang ? <Container Title={"Reacting to input with state"}>{DataText[2].Reactingtoinput()} </Container>: <Container Title="واکنش به ورودی با استفاده از state" Direction="rtl">{DataText[3].ReactingToInputFa()}</Container>}
      
       <CityQiuz />
       <CityQuiz2 />
-      <Container Title={"Choosing the state structure"}>
-        {DataText[4].ChoosingTheState()}
-      </Container>
-      <Container Direction={"rtl"} Title={"Choosing the state structure"}>
-          {DataText[5].ChoosingTheStateFa()}
-      </Container>
+      {lang ? <Container Title={"Choosing the state structure"}>{DataText[4].ChoosingTheState()}</Container> :<Container Direction={"rtl"} Title={"   انتخاب ساختار  state"}>{DataText[5].ChoosingTheStateFa()}
+      </Container>}
       <FullName />
-      <Container Title={"Sharing state between components"}>
-        {DataText[6].SharingState()}
-      </Container>
-      <Container Title={"Sharing state between components"} Direction={"rtl"}>
-        {DataText[7].SharingStateFa()}
-      </Container>
+      {lang ? <Container Title={"Sharing state between components"}>{DataText[6].SharingState()}</Container> :
+      <Container Title={"Sharing state between components"} Direction={"rtl"}>{DataText[7].SharingStateFa()}</Container>}
       <Panel />
-      <Container Title="Preserving and resetting state ">
-        {DataText[8].PreservingAndResetting()}
-      </Container>
+      {lang ? <Container Title="Preserving and resetting state ">{DataText[8].PreservingAndResetting()}</Container> :
+       <Container Title="state حفظ و تنظیم مجدد ">{DataText[9].PreservingAndResettingFa()}</Container>}
       <ChatList />
-      <Container Title="Extracting state logic into a reducer ">
-        {DataText[9].ExtractingStateLogic()}
-      </Container>
-      <Container
-        Direction={"rtl"}
-        Title="Extracting state logic into a reducer "
-      >
-        {DataText[10].ExtractingStateLogicFa()}
-      </Container>
+      {lang ?<Container Title="Extracting state logic into a reducer ">{DataText[10].ExtractingStateLogic()}</Container>:
+      <Container Direction={"rtl"} Title="استخراج منطق stage در یک کاهنده ">{DataText[11].ExtractingStateLogicFa()}</Container>}
       <Title title="React Context" />
-      <Container Title="What is React Context?">
-        {DataText[13].WhatIsReactContext()}
-      </Container>
-      <Container Title="How is React Context Different from Prop Threading?">
-        {DataText[14].ReactContextDifferent()}
+      {lang ? <Container  Title="What is React Context ?">{DataText[14].WhatIsReactContext()}</Container> :
+      <Container Direction={"rtl"} Title="چیست؟ React Context ">{DataText[15].WhatIsReactContextFa()}</Container>}
+      {lang ?<Container Title="How is React Context Different from Prop Threading?">
+        {DataText[16].ReactContextDifferent()}
         <img
           src={imgReactContext}
           className="w-full h-full mt-4"
           alt="React Context"
         />
-      </Container>
+      </Container>:
+      <Container Direction={"rtl"} Title="React Context چه تفاوتی با Prop Threading دارد؟">
+        {DataText[17].ReactContextDifferentFa()}
+        <img
+          src={imgReactContext}
+          className="w-full h-full mt-4"
+          alt="React Context"
+        />
+      </Container>}
       <Container Title="Toggle Theme">
         <ToggleTheme />
       </Container>
-      <Container Title="Passing data deeply with context  ">
-        {DataText[11].PassingDataDeeplyWithContext()}
-      </Container>
+      {lang ?<Container Title="Passing data deeply with context ">{DataText[12].PassingDataDeeplyWithContext()}</Container>:
+      <Container Direction={"rtl"} Title="Passing data deeply with context  "> {DataText[13].PassingDataDeeplyWithContextFa()}</Container>}
       <PassingDataNesting />
-      <Container Direction={"rtl"} Title="Passing data deeply with context  ">
-        {DataText[12].PassingDataDeeplyWithContextFa()}
-      </Container>
       <NoteReducer />
       <Container>
         <CardImport />
