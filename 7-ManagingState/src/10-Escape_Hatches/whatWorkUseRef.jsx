44de9111ea30745b594f676b1c6de08e481e095f
mohmmadView <mamad.rahgoshahi@gmail.com>
2024-03-06 04:43:30 +0330

    create WhatWorkUseRef

--- 7-ManagingState/src/10-Escape_Hatches/whatWorkUseRef.jsx
+++ 7-ManagingState/src/10-Escape_Hatches/whatWorkUseRef.jsx
@@ -3,16 +3,20 @@ export default function WhatWorkUseRef(lang) {
 
 
     return(
-        <div className="w-full  mx-auto my-4 bg-base-300 p-10 text-xl">
-        <div className="collapse ">
-  <input style={{opacity:1}} type="checkbox" className="peer  w-20 h-14 absolute right-14 checkbox checkbox-primary opacity-100" /> 
-  <div className="w-full collapse-title border border-error  bg-error-content h-auto p-8 text-primary-content peer-checked:bg-primary-content peer-checked:text-primary">
-    <p className="text-sm text-blue-300  pb-3">better understanding </p>
- <p className="text-xl text-error"> How does useRef work inside? </p>
+       
+        <div className="collapse w-full border border-primary rounded-box mt-8">
+  <input   type="checkbox" className="peer  " /> 
+  <div  className="flex justify-between collapse-title   border border-error  bg-error-content text-primary-content peer-checked:bg-primary-content peer-checked:text-primary">
+   <div>
+      <p className=" p-2 text-sm text-blue-300  max-md:text-xs">better understanding </p>
+   <p className="  text-xl max-md:text-lg text-error"> How does useRef work inside? </p>
+   </div>
+<p className="text-center font-bold mr-28 mt-3 p-2 text-sm text-warning  max-md:text-xs"> click more</p>
   </div>
-  <div className="collapse-content bg-primary-content text-error-content peer-checked:bg-warning peer-checked:text-warning"> 
-   <div className="w-11/12 mx-auto mt-2 ">
-  <Code  fileName={`useRef(initialValue)`} widthIN={` w-full m-auto h-auto `} language={'js'} code={`
+  <div className="collapse-content bg-primary-content text-error-content  peer-checked:bg-warning peer-checked:text-warning"> 
+   <div className=" mt-2 ">
+  <Code fileName={`useRef(initialValue)`}  language={'js'} widthIN={`max-sm:w-[62%]  max-md:w-[79%] max-md:m-0 max-lg:w-11/12 max-lg:m-0 h-auto m-auto`}
+  code={`
 // Inside of React
 function useRef(initialValue) {
   const [ref, unused] = useState({ current: initialValue });
@@ -23,6 +27,6 @@ function useRef(initialValue) {
   </div>
 </div>
         
-        </div>
+  
     )
 }
