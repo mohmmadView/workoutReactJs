4c3c25033b64a10fa5cedb0f8d691c3652fdb501
mohmmadView <mamad.rahgoshahi@gmail.com>
2024-04-24 23:16:13 +0330

    how to write effect update-2

--- 7-ManagingState/src/11-Effects/HowToWriteAnEffect.jsx
+++ 7-ManagingState/src/11-Effects/HowToWriteAnEffect.jsx
@@ -1,7 +1,6 @@
 import DataText from "../TasksContext"
 import Video from "./mediaPlayer"
 export default function HowToWriteAnEffect (lang) {
-
     return (
       <div className="my-12">
         {lang.lang ? (
@@ -10,7 +9,7 @@ export default function HowToWriteAnEffect (lang) {
           <>{DataText[25].HowToWriteAnEffectFa()}</>
         )}
         <div className="divider divider-start text-accent mt-8 divider-secondary ">
-          example : <span > Media Player</span>
+          example : <span> Media Player</span>
         </div>
         <Video  />
       </div>
--- 7-ManagingState/src/11-Effects/mediaPlayer.jsx
+++ 7-ManagingState/src/11-Effects/mediaPlayer.jsx
@@ -1,4 +1,5 @@
 import { useState, useRef, useEffect } from 'react';
+import Code from '../utils/Prism';
 
 function VideoPlayer({ src, isPlaying }) {
   const ref = useRef(null);
@@ -14,15 +15,54 @@ return <video ref={ref} src={src} loop playsInline />;
 export default function App() {
   const [isPlaying, setIsPlaying] = useState(false);
   return (
-    <div className='w-1/2' >
-     
-      <VideoPlayer
-        isPlaying={isPlaying}
-        src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
-      />
-       <button className='btn w-full btn-secondary' onClick={() => setIsPlaying(!isPlaying)}>
-        {isPlaying ? 'Pause' : 'Play'}
-      </button>
-    </div>
+   <div className='flex' >
+        <div className='w-1/2' >
+          <VideoPlayer
+            isPlaying={isPlaying}
+            src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
+          />
+           <button className='btn w-full btn-secondary' onClick={() => setIsPlaying(!isPlaying)}>
+            {isPlaying ? 'Pause' : 'Play'}
+          </button>
+        </div>
+         <div className='w-1/2'>
+               <Code more={true} code={`
+   import { useState, useRef, useEffect } from 'react';
+import Code from '../utils/Prism';
+
+function VideoPlayer({ src, isPlaying }) {
+  const ref = useRef(null);
+useEffect(() => {
+  if (isPlaying) {
+    ref.current.play();  // Calling these while rendering isn't allowed.
+  } else {
+    ref.current.pause(); // Also, this crashes.
+}
+})
+return <video ref={ref} src={src} loop playsInline />;
+}
+export default function App() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  return (
+   <div className='flex' >
+        <div className='w-1/2' >
+          <VideoPlayer
+            isPlaying={isPlaying}
+            src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
+          />
+           <button className='btn w-full btn-secondary' onClick={() => setIsPlaying(!isPlaying)}>
+            {isPlaying ? 'Pause' : 'Play'}
+          </button>
+        </div>
+       
+   </div>
+  );
+}
+
+     `} language={"js"}
+              widthIN={"w-full  "} ></Code>
+           
+         </div>
+   </div>
   );
 }
