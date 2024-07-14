90773c5389e7b9b4b8c8143a9a3a076e3f2fb894
mohmmadView <mamad.rahgoshahi@gmail.com>
2024-07-15 03:15:37 +0330

    upde custo hooks

--- 7-ManagingState/src/5-CustomHooks/index.jsx
+++ 7-ManagingState/src/5-CustomHooks/index.jsx
@@ -12,11 +12,15 @@ export default function CustomHooks({lang}){
             <IsOnline />
                {DataText.customHooks2()}
                <CustomHookInput/>
+               <CustomHookInput/>
+               <CustomHookInput/>
            </Container>
                : <Container  Title="استفاده از قطعی برای استفاده از Custom Hooks" ColorDivider={`success`} ColorText={`warning text-pink-200`}>
                {DataText.ReusingLogicWithCustomHooksFa()}
                <IsOnline />
                {DataText.customHooks2Fa()}
+               <CustomHookInput/>
+               <CustomHookInput/>
                <CustomHookInput/>
                </Container>
             }
